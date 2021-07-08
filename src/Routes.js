a3dbe706eed439ad6502e710dfce76b55221ad8f
JD Fillmore <jd@jdfill.me>
2021-07-07 23:12:42 -0400

    Comment out Routes file as not need at moment

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import App from "./App";
-import MachineDetail from "./components/MachineDetail";
+// import React from "react";
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import App from "./App";
+// import MachineDetail from "./components/MachineDetail";
 
-const Routes = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route exact path="/details" component={MachineDetail} />
-      </Switch>
-    </BrowserRouter>
-  );
-};
+// const Routes = () => {
+//   return (
+//     <BrowserRouter>
+//       <Switch>
+//         <Route exact path="/" component={App} />
+//         <Route path="/details/:id" component={MachineDetail} />
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// };
 
-export default Routes;
+// export default Routes;
