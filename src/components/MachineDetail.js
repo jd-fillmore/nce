60d3f148ac2c17ed3e4ba4c8ed5b3a6a471770e8
JD Fillmore <jd@jdfill.me>
2021-07-07 23:13:38 -0400

    Add isLoading logic

--- src/components/MachineDetail.js
+++ src/components/MachineDetail.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useAPI } from "../context/apiContext";
 import Header from "../components/Header";
 
-export default function MachineDetail({ image, title, weight, power }) {
+export default function MachineDetail() {
+  // Load in global state
+  const { data, isLoading } = useAPI();
   return (
     <>
       <Header title="Product title" subTitle="Product category" />
-      <div className="card">
-        <h2>Machine Detail</h2>
-        <p>Machine detail content goes here</p>
-      </div>
+      {!isLoading ? (
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <img src={data.model.image} alt="" />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
