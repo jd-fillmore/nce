0b4b17a6feff97f4e05d19adc92d6dbbe342f98d
JD Fillmore <jd@jdfill.me>
2021-07-13 13:34:13 -0400

    Add in ContextProvider to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import APIContextProvider from "./context/apiContext";
 import Header from "./components/Header";
 import "../src/scss/main.scss";
 import SearchFilter from "./components/SearchFilter";
 
 function App() {
   return (
-    <APIContextProvider>
-      <div className="App">
-        <Header
-          title="New Cat Equipment"
-          subTitle="Search for our new Cat equipment stock"
-        />
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <SearchFilter />
-            </div>
+    <div className="App">
+      <Header
+        title="New Cat Equipment"
+        subTitle="Search for our new Cat equipment stock"
+      />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <SearchFilter />
           </div>
         </div>
       </div>
-    </APIContextProvider>
+    </div>
   );
 }
 
